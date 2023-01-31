ad0c312187937e7bcff814d9ccad4f6bfc1cecaa
D3vL4dy <kangji1218@kakao.com>
2023-01-31 20:34:06 +0900

    문자열

---
+++ 문자열/Main_11720_숫자의합.java
@@ -0,0 +1,23 @@
+package backJoon;
+
+import java.util.Scanner;
+
+/*
+ * N개의 숫자가 공백 없이 쓰여있다. 
+ * 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+ */
+
+public class Main_11720_숫자의합 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		String num = sc.next();
+		int sum = 0;
+		
+		for(int i=0; i<n; i++) {
+			sum += num.charAt(i)-'0';
+		}
+		System.out.println(sum);
+	}
+}
