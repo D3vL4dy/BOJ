b8952b2161fd5e75415dba7508353907ed6b38d1
D3vL4dy <kangji1218@kakao.com>
2023-06-14 21:23:44 +0900

    팰린드롬인지 혹인하기

---
+++ 심화1/Main_10988.java
@@ -0,0 +1,25 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main_10988 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+        String str = br.readLine();
+        int len = str.length();
+        int flag = 1;
+        
+		for (int i = 0; i < len/2; i++) {
+            if(str.charAt(i)!=str.charAt(len-i-1)){
+                flag = 0;
+                break;
+            }
+		}
+        System.out.println(flag);
+	}
+}
