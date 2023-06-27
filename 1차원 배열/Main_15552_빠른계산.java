07fbe4aa09e42bd3698d20321788952b334e3222
D3vL4dy <kangji1218@kakao.com>
2023-06-27 21:29:02 +0900

    배열

---
+++ 1차원 배열/Main_15552_빠른계산.java
@@ -0,0 +1,29 @@
+package prac;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main_15552_빠른계산 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int cnt = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<cnt; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int sum = a+b;
+			
+			bw.write(sum + "\n");
+		}
+		bw.flush();
+	}
+
+}
