17afe534bb34b97299b1a2b9e0b1bf308eb8459a
D3vL4dy <kangji1218@kakao.com>
2023-08-12 22:10:04 +0900

    수 정렬하기1

--- 정렬/Main_2750.java
+++ 정렬/Main_2750.java
@@ -4,9 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main_2750 {
+public class Main_2750 { // 삽입정렬
 
-	public static void main(String[] args) throws IOException { // 삽입정렬
+	public static void main(String[] args) throws IOException { 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
@@ -33,3 +33,36 @@ public class Main_2750 {
 	}
 
 }
+
+/*
+public class Main_2750 { // 선택정렬
+
+	public static void main(String[] args) throws IOException { 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		for (int i = 0; i < n - 1; i++) {
+			int min_idx = i;
+			for (int j = i + 1; j < n; j++) {
+				if (arr[j] < arr[min_idx]) {
+					min_idx = j;
+				}
+			}
+			int temp = arr[min_idx];
+			arr[min_idx] = arr[i];
+			arr[i] = temp;
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+}
+*/
