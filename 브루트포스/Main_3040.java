090b5b4cc6bced819aed02eeb3acf5b78a10960a
D3vL4dy <kangji1218@kakao.com>
2023-08-16 21:10:21 +0900

    백설 공주와 일곱 난쟁이

---
+++ 브루트포스/Main_3040.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_3040 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int arr[] = new int[9];
+		int sum = 0;
+		int num1 = 0, num2 = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (sum - arr[i] - arr[j] == 100) {
+					num1 = i;
+					num2 = j;
+				}
+			}
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			if (i != num1 && i != num2) {
+				System.out.println(arr[i]);
+			}
+		}
+	}
+
+}
